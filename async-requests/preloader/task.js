92de007917a796bf35c0533284b78b13dca4443d
VNKirichenko <VNKirichenko@inno.tech>
2026-03-02 09:05:31 +0300

    preloader

---
+++ async-requests/preloader/task.js
@@ -0,0 +1,38 @@
+let loader = document.querySelector('#loader')
+let items = document.querySelector('#items')
+
+function render(currencies = []) {
+  if (currencies.length === 0) {
+    items.innerHTML = '<div>Данных не найдено ...</div>'
+  } else {
+    items.innerHTML = currencies.map(i => `
+      <div class="item">
+        <div class="item__code">
+          ${i.CharCode}
+        </div>
+        <div class="item__value">
+          ${i.Value}
+        </div>
+        <div class="item__currency">
+            руб.
+        </div>
+      </div>`
+    ).join('')
+  }  
+}
+
+async function getData() {
+    try {
+      const response = await fetch('https://students.netoservices.ru/nestjs-backend/slow-get-courses')
+      data = await response.json()
+      loader.classList.remove('loader_active')
+      console.log(data)
+      console.log(Object.values(data.response.Valute))
+      render(Object.values(data.response.Valute))
+    } catch (e) {
+      throw e
+    }
+  }
+
+
+  getData()
